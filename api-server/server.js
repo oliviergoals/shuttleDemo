08f19e5fb4b6b320de9a669ec3a4b169afb5adec
Sean Lim <seanmichael_lim@mymail.sutd.edu.sg>
2021-02-24 14:15:44 +0800

    add uploading feature

--- api-server/server.js
+++ api-server/server.js
@@ -3,14 +3,54 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 var visionRouter = require("./routes/vision");
+let multer = require("multer");
+let fs = require("fs");
+let storage_mult = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, "public")
+  },
+  filename: function (req, file, cb) {
+      const parts = file.mimetype.split("/");
+      cb(null, `${file.fieldname}.${parts[1]}`)
+  }
+})
+let upload = multer({ storage: storage_mult })
+const app = express();
+const {Storage} = require('@google-cloud/storage');
+const serviceAccount = require('/Users/thebe/Desktop/shuttleDemo/json-key/shuttleOneVisionToken.json');
+// Creates a client
+// const storage = new Storage();
+const storage = new Storage({
+  projectId:serviceAccount.project_id,
+  credentials: serviceAccount});
+const bucketName = 'testing_pdf123';
+const myBucket = storage.bucket(bucketName);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({limit:'50mb', extended: true }));
+app.use(bodyParser.json({limit:'50mb', extended: true }));
+app.set('json spaces', 2);
+
+// async function createBucket() {
+//   await storage.createBucket(bucketName);
+//   console.log(`Bucket ${bucketName} created.`);
+// }
+
+// createBucket().catch(console.error);
+
+app.post('/submit',upload.single('myFile'), async (req,res)=> {
+  console.log("SERVER REACHED // SUBMITTING PDF FILE")
+  console.log(req.file,req.body);
+  upload(req, res, function (err) {
+    return res.status(200).send(req.file)
+  })
+});
+
 
 //Load route files here
 //Default index file, can be removed
-const app = express();
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 
 //const meta = mongoose.createConnection(mongoURIMeta, {useNewUrlParser: true})
 // const logs = mongoose.createConnection(mongoURILogs,{useNewUrlParser: true}).then(() => console.log('MongoDB Log Connected...'))
@@ -18,9 +58,8 @@ app.use(bodyParser.json());
 
 //Use routes here
 app.use('/vision', visionRouter);
+
 const port = process.env.PORT || 8080;
-
-
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -1,8 +1,54 @@
 import Head from 'next/head'
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 import GoogleVisionApi from './api/GoogleVisionAPI'
+import axios from 'axios';
+let url = "http://localhost:8080"
+const fetch = require("node-fetch");
+
 
 export default function Home() {
+  const [file, setFile] = useState();
+  const [fileName,setFileName] = useState()
+  const [uri,setUri] = useState()
+
+  async function onSubmit(e){
+    e.preventDefault();
+    // const data = new FormData() 
+    // data.append('file', file)
+    console.log(file)
+    // const formData = new FormData();
+    // formData.append()
+    // try{
+    //   let result = await fetch(url + "/submit", {
+    //     method: 'POST',
+    //     headers: {
+    //     'Content-Type': "application/json"
+    //     },
+    //     body:JSON.stringify({file:"file"})
+    //   })
+    //   let result_json = await result.json();
+    //   console.log('Success:', result_json);
+    //   return result_json;
+    // }
+    //   catch(error) {
+      //     console.error('Error:', error);
+      //     return error;
+      //   };
+      try{
+        const data = new FormData() 
+        data.append('file', file)
+        console.log(data);
+        let result =  await axios.post(url + "/submit", data, {})
+        console.log(result);
+        console.log('Success:', result);
+        return result;
+    }
+    catch(e){
+      console.log(e)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,11 +57,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <form>
+      <form  
+      onSubmit={onSubmit}
+      // action="https://localhost:8000/submit"
+      // method="post" 
+      encType="multipart/form-data">
         <label>Select a file:</label>
-        <input type="file" id="myfile" name="myfile"></input>
+        <input type="file" id="myFile" name="myFile" 
+        onChange={e => {setFileName(e.target.value);
+          setFile(e.target.files[0]);
+          let splits = String(e.target.value).split('\\') ;
+          const str = "gs://testing_pdf123/" + splits[splits.length - 1];
+          setUri(str) 
+        }}>
+
+        </input>
         <input type="submit"></input>
       </form>
+
+      <div style={{padding:"10px"}}>
+        URI Value: {uri}
+      </div>
+      
       <button onClick={GoogleVisionApi.test}>
         Test API
       </button>
